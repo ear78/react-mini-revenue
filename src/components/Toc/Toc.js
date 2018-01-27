8cf77f5e80e0d9e272de41accaca10dc259fa6d9
elliot richardson <ear78@aol.com>
2018-01-26 20:38:25 -0600

    backing up files from late night session

--- src/components/Toc/Toc.js
+++ src/components/Toc/Toc.js
@@ -4,6 +4,9 @@ import overview from '../../overview';
 import mockData from '../../mockData';
 import './Toc.css';
 
+import Content from './Content/Content';
+import TableOfContents from './TableOfContents/TableOfContents';
+
 class Toc extends React.Component{
     constructor(){
         super();
@@ -16,13 +19,14 @@ class Toc extends React.Component{
             return <li key={i}>{d.product}</li>
         })
 
-        const tableOfContents = this.state.data.map((d,i) => {
+        const tableOfContentsList = this.state.data.map((d,i) => {
             return <li key={i}>{d.product}
                         <ol className="sub">
                             <li>{d.table}</li>
                         </ol>
                     </li>
         })
+
         return (
             <main className="main-container">
                 <nav className="nav-left">
@@ -31,16 +35,13 @@ class Toc extends React.Component{
                     </ul>
                 </nav>
                 <section className="section-right">
-                    <div className="content">
-                        <h1>Fake Heading</h1>
-                        <h4>Fake Sub Heading title</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
+                    <Content />
 
                     <aside className="toc">
                         <h4>Table Of Contents</h4>
                         <ol>
-                            {tableOfContents}
+
+                            {tableOfContentsList}
                         </ol>
                     </aside>
                 </section>
