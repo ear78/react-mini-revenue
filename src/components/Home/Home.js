dd35c40916ad60631edf8cb7b82a5adf74b4fdc7
elliot richardson <ear78@aol.com>
2018-02-13 21:40:02 -0600

    commented out modal component temporarily

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
+
 import './Home.css';
 import Line from '../UI/Line/Line';
 import Modal from '../UI/Modal/Modal';
@@ -7,7 +9,8 @@ class Home extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            showBlock: false
         }
     }
 
@@ -25,9 +28,32 @@ class Home extends React.Component{
                 <p>
                     Please <span>navigate</span> through the <span className="app-span">app</span> to see the different mini projects.
                 </p>
-                <button onClick={this.showModal} style={{width: '160px', display: 'block', margin: '0 auto'}} className="button tiny">Click For Modal</button>
-                <Modal
-                    show={this.state.modalIsOpen} close={this.hideModal}/>
+                {/*
+                    <button onClick={this.showModal} style={{width: '160px', display: 'block', margin: '0 auto'}} className="button tiny">Click For Modal</button>
+                    <Transition in={this.state.modalIsOpen} timeout={300}>
+                        {state => (
+                            <Modal
+                                show={this.state.modalIsOpen} close={this.hideModal}/>
+                        )}
+                    </Transition>
+                */}
+
+
+                {/*
+                    <button className="button tiny" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+                    <Transition in={this.state.showBlock} timeout={300}>
+                        {state => (<div style={{
+                            width: '90%',
+                            height: '200px',
+                            backgroundColor: 'lightblue',
+                            transition: 'opacity 1s ease-out',
+                            opacity: state === 'exited' ? 0 : 1
+                        }}></div>)}
+
+                    </Transition>
+                */}
+
+
             </div>
 
         )
